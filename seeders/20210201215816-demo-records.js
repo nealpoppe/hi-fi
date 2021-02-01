e5c16670571fbb7fbca4a32a2e1757613eb51873
nealpoppe <npoppe@gmail.com>
2021-02-01 16:10:29 -0600

    adding files

---
+++ seeders/20210201215816-demo-records.js
@@ -0,0 +1,36 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+
+     await queryInterface.bulkInsert('People', [
+       {
+         artist: "Motley Crue",
+         title: "Girls, Girls Girls"
+       },
+       {
+        artist: "Motley Crue",
+        title: "Dr. Feelgood"
+      },
+      {
+        artist: "Motley Crue",
+        title: "Shout at the Devil"
+      },
+      {
+        artist: "Motley Crue",
+        title: "Theatre of Pain"
+      }
+
+     ], {});
+
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
