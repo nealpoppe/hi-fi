306391c2bb0ccf63030a4a55702b6d40f9071605
vincentm <matthiasvincentm@johndeere.com>
2021-02-02 10:56:31 -0600

    controllers index.js exports and users controller and routes index

---
+++ controllers/index.js
@@ -0,0 +1,4 @@
+module.exports = {
+    records: require('./records'),
+    users: require('./users')
+}
--- routes/users.js
+++ routes/users.js
@@ -1,1 +1,9 @@
 const express = require('express');
+const router = express.Router();
+const ctrl = require('../controllers');
+
+router.get('/', ctrl.users.index);
+
+
+
+module.exports = router;
