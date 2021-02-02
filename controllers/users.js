9e61cd1eb4ca67c4a1af6cb829489d7808f7fc59
vincentm <matthiasvincentm@johndeere.com>
2021-02-02 15:38:49 -0600

    signup and login routes added, working on rendering profile page

--- controllers/users.js
+++ controllers/users.js
@@ -1,20 +1,55 @@
-const User = require('../models').User;
+const User = require("../models").User;
 
 const index = (req, res) => {
-    res.render('users/index.ejs')
+    res.render("users/index.ejs")
 };
 
+
 const renderSignup = (req, res) => {
-    res.render('users/signup.ejs')
+    res.render("users/signup.ejs")
 }
+const signup = (req, res) => {
+    User.create(req.body)
+    .then(newUser => {
+        console.log(newUser);
+        res.redirect(`/users/profile/${newUser.id}`);
+    })
+};
+
 
 const renderLogin = (req, res) => {
-    res.render('users/login.ejs')
+    res.render("users/login.ejs")
 }
+const login = (req, res) => {
+    User.findOne({
+        where: {
+            username: req.body.username,
+            password: req.body.password
+        }
+    })
+    .then(foundUser => {
+        res.redirect(`/users/profile/${foundUser.id}`);
+    })
+};
+
+const renderProfile = (req, res) => {
+    User.findByPk(req.params.id)
+    .then(userProfile => {
+        res.render("users/profile.ejs", {
+            user: userProfile
+        })
+     })
+};
+
+
+
 
 module.exports = {
     index,
     renderSignup,
+    signup,
     renderLogin,
-    
+    login,
+    renderProfile,
+
 }
--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,12 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const ctrl = require('../controllers');
+const ctrl = require("../controllers");
 
-router.get('/', ctrl.users.index);
-router.get('/signup', ctrl.users.renderSignup);
-
-router.get('/login', ctrl.users.renderLogin);
-
+router.get("/", ctrl.users.index);
+router.get("/signup", ctrl.users.renderSignup);
+router.post("/signup", ctrl.users.signup);
+router.get("/login", ctrl.users.renderLogin);
+router.post("/login", ctrl.users.login);
+router.get("/profile/:id", ctrl.users.renderProfile);
 
 module.exports = router;
