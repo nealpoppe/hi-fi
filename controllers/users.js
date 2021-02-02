d121a14dc3cb147e745d5d45e142ecb8de0b5a02
vincentm <matthiasvincentm@johndeere.com>
2021-02-02 14:28:52 -0600

    add routes controllers for render signup and login

--- controllers/users.js
+++ controllers/users.js
@@ -4,9 +4,17 @@ const index = (req, res) => {
     res.render('users/index.ejs')
 };
 
+const renderSignup = (req, res) => {
+    res.render('users/signup.ejs')
+}
 
+const renderLogin = (req, res) => {
+    res.render('users/login.ejs')
+}
 
 module.exports = {
     index,
-   
+    renderSignup,
+    renderLogin,
+    
 }
--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,9 @@ const router = express.Router();
 const ctrl = require('../controllers');
 
 router.get('/', ctrl.users.index);
+router.get('/signup', ctrl.users.renderSignup);
 
+router.get('/login', ctrl.users.renderLogin);
 
 
 module.exports = router;
