909f3e9b02536bced538f2d045d77806d48a2657
nealpoppe <npoppe@gmail.com>
2021-02-02 15:26:38 -0600

    record edit and delete routes and controllers

--- controllers/records.js
+++ controllers/records.js
@@ -20,6 +20,7 @@ const show = (req, res) => {
         }]
     })
     .then(record => {
+        console.log(req.params);
         res.render('show.ejs', {
             record: record
         });
@@ -30,13 +31,52 @@ const show = (req, res) => {
 const renderNew = (req, res) => {
     res.render("new.ejs");
 }
+
 // postRecord
+const postRecord = (req, res) => {
+    Record.create(req.body)
+    .then(newRecord => {
+        res.redirect("record") // not sure about this
+    })
+}
+
 // renderEdit
+const renderEdit = (req, res) => {
+    Record.findByPK(req.params.id)
+    .then(record => {
+        res.render("edit.ejs", {
+            record: record
+        });
+    })
+}
+
 // editRecord
+const editRecord = (req,res) => {
+    Record.update(req.body, {
+        where: { id: req.params.id },
+        returning: true
+    })
+    .then(record => {
+        res.redirect('/record');
+    })
+}
+
 // deleteRecord
+const deleteRecord = (req, res) => {
+    Record.destroy({
+        where: { id: req.params.id}
+    })
+    .then(() => {
+        res.redirect('/pokemon');
+    })
+}
 
 module.exports = {
     index,
     show,
     renderNew,
+    postRecord,
+    renderEdit,
+    editRecord,
+    deleteRecord
 }
--- routes/records.js
+++ routes/records.js
@@ -4,6 +4,10 @@ const ctrl = require('../controllers');
 
 router.get('/', ctrl.records.index); // index
 router.get("/new", ctrl.records.renderNew); // renderNew
+router.post("/", ctrl.records.postRecord); // postRecord
 router.get("/:id", ctrl.records.show); // show
+router.get("/:id/edit", ctrl.records.renderEdit); // renderEdit
+router.put("/:id", ctrl.records.editRecord); // editRecord
+router.delete("/:id", ctrl.records.deleteRecord);
 
 module.exports = router;
